7e65c19dd6d344547cd1d91c7f27e19cc30b6bdc
DeadAnjali <anjalimahere15@gmail.com>
2024-06-20 21:23:29 +0530

    user-message-schema

---
+++ src/model/User.ts
@@ -0,0 +1,63 @@
+import mongoose,{Schema,Document} from "mongoose";
+//definign interfaces for Message, User and uploaded documents
+
+export interface Message extends Document{
+    content:string,
+    createdAt:Date
+}
+
+export interface User extends Document{
+    username:string,
+    email:string,
+    password:string,
+    verifyCode:string,
+    verifyCodeExpiry:Date,
+    isAcceptingMessage:boolean,
+    message:Message[]
+}
+
+const MessageSchema: Schema<Message>=new Schema({
+    content:{
+        type:String,
+        required:true
+    },
+    createdAt:{
+        type:Date,
+        required:true,
+        default:Date.now
+    }
+})
+
+const UserSchema:Schema<User>=new Schema({
+    username:{
+        type:String,
+        required:[true,"Username is required"],
+        unique:true
+    },
+    email:{
+        type:String,
+        required:[true,"email is required"],
+        unique:true
+    },
+    password:{
+        type:String,
+        required:[true,"Username is required"],
+        unique:true
+    },
+    verifyCode:{
+        type:String,
+        required:[true,"Username is required"],
+        unique:true
+    },
+    verifyCodeExpiry:{
+        type:Date,
+        required:[true,"Username is required"],
+        unique:true
+    },
+    isAcceptingMessage:{
+        type:Boolean,
+        required:[true,"Username is required"],
+        unique:true
+    },
+    message:[MessageSchema]
+})
